6babf90101febe973e20cf04c44adfd20b01d8a5
yjc <rigorcheng@163.com>
2023-08-25 18:12:43 +0800

    master2 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello get2222!");
         System.out.println("hello get33333!");
         System.out.println("hello hot_fixx!");
+        System.out.println("hello hot_fixx222!");
     }
 }
