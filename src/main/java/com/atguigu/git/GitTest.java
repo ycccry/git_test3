43ca0e8c48332b40a88a950ec36d2a4351be52c3
yjc <rigorcheng@163.com>
2023-08-25 18:53:54 +0800

    push commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello hot_fixx!");
         System.out.println("hello master!");
         System.out.println("hello hot_fixx222!");
+        System.out.println("push test");
     }
 }
